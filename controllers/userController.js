d350dd53b05b54f26217eb435d5a71ec87b3c773
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2021-05-11 23:33:04 +0700

    feat: add update delete  data yo mysql

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const bcryptjs = require('bcryptjs')
 const models = require('../models/index')
 
 exports.index = async (req, res, next) => {
@@ -62,22 +63,27 @@ exports.show = async (req, res, next) => {
     }
 }
 
+//insert
 exports.insert = async (req, res, next) => {
     try {
         const { name, email, password } = req.body
         //check email ซ้ำ
         const existEmail = await models.User.findOne({ where: { email: email } })
-        if(existEmail){
-            const error = new Error('ไม่พบผู้ใช้นี้ในระบบ **email ซ้ำ' )
+        if (existEmail) {
+            const error = new Error('ไม่พบผู้ใช้นี้ในระบบ **email ซ้ำ')
             error.statusCode = 400
             throw error
         }
-        
+
+        //hash password
+        const salt = await bcryptjs.genSalt(8)
+        const passwordHash = await bcryptjs.hash(password, salt)
+
         //insert user
         const user = await models.User.create({
             name,
             email,
-            password,
+            password: passwordHash,
         })
         res.status(200).json({
             message: 'เพิ่มข้อมูลเรียบร้อยแล้ว',
@@ -85,6 +91,76 @@ exports.insert = async (req, res, next) => {
                 id: user.id,
                 email: user.email,
             },
+        })
+    } catch (error) {
+        res.status(error.statusCode).json({
+            error: {
+                message: error.message,
+            },
+        })
+    }
+}
+
+//update
+exports.update = async (req, res, next) => {
+    try {
+        const { id, name, email, password } = req.body
+
+        if (req.params.id !== id) {
+            const error = new Error('รหัสผู้ใช้ไม่ถูกต้อง')
+            error.statusCode = 400
+            throw error
+        }
+
+        //hash password
+        const salt = await bcryptjs.genSalt(8)
+        const passwordHash = await bcryptjs.hash(password, salt)
+
+        //update user
+        const user = await models.User.update(
+            {
+                name,
+                email,
+                password: passwordHash,
+            },
+            {
+                where: {
+                    id: id,
+                },
+            }
+        )
+        res.status(200).json({
+            message: 'แก้ไขข้อมูลเรียบร้อยแล้ว',
+        })
+    } catch (error) {
+        res.status(error.statusCode).json({
+            error: {
+                message: error.message,
+            },
+        })
+    }
+}
+
+//delete
+exports.delete = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const user = await models.User.findByPk(id) //find by primary key
+        if (!user) {
+            const error = new Error('ไม่พบผู้ใช้นี้ในระบบ')
+            error.statusCode = 404
+            throw error
+        }
+
+        //delete user by id
+        await models.User.destroy({
+            where: {
+                id: id,
+            },
+        })
+
+        res.status(200).json({
+            message: 'ลบข้อมูลเรียบร้อยแล้ว',
         })
     } catch (error) {
         res.status(error.statusCode).json({
--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,15 @@ const userController = require('../controllers/userController')
 /* GET users listing. */
 router.get('/', userController.index)
 
+//get one
 router.get('/:id', userController.show)
 
+//insert
 router.post('/', userController.insert)
 
+//update
+router.put('/:id', userController.update)
+
+//delete
+router.delete('/:id', userController.delete)
 module.exports = router
